d8bb000c2f5448abb4ef297d2e2e6d9ef7a0c497
EJBEAN <64283902+itsAnanth@users.noreply.github.com>
2021-10-27 23:02:22 +0530

    Create bubble sort.c

---
+++ WEEK 1/bubble sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void bubbleSort(int * array, int size);
+void swap(int & a, int & b);
+
+
+int main() {
+    int array[10] = { 2, 5, 10, 12, 15, 24, -1, 4, 9, 11 };
+    int size = sizeof(array)/sizeof(array[0]);
+    int i = 0;
+    bubbleSort(array, size);
+    for (i; i < size; i++)
+        printf("%d ", array[i]);
+}
+
+void bubbleSort(int * array, int size) {
+    int i, j;
+    for (i = 0; i < size; i++) {
+        int swaps = 0;
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                swap(array[j], array[j + 1]);
+                swaps++;
+            };
+        }
+        if (!swaps) break;
+    }
+}
+
+void swap(int & a, int & b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
