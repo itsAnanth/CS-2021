d7e19b0bb149a9a72a84a1fe8f75715287111561
EJBEAN2op <danonymousarmy@gmail.com>
2021-10-28 15:56:16 +0530

    better UI

--- WEEK 1/dynamic memory.c
+++ WEEK 1/dynamic memory.c
@@ -3,16 +3,15 @@
 
 int main() {
     int n, i;
-    printf("Enter number of elements to allocate :");
+    printf("Enter number of elements to allocate : ");
     while(scanf("%d", &n)) {
         int * ptr = (int*)calloc(n, sizeof(int));
         for (i = 0; i < n; i++)
-            ptr[i] = i;
+            ptr[i] = i + 1;
         for (i = 0; i < n; i++)
             printf("%d ", ptr[i]);
+        printf("\n");
         free(ptr);
-        for (i = 0; i < n; i++)
-            printf("%d ", ptr[i]);
     }
     return 0;
 
