0b150366767dc00135831fc7aaeb37ce7dc32215
EJBEAN2op <danonymousarmy@gmail.com>
2021-11-01 16:05:44 +0530

    function reference

--- WEEK 1/function pointers.cpp
+++ WEEK 1/function pointers.cpp
@@ -1,19 +1,25 @@
 #include <bits/stdc++.h>
 
-void forEach(const std::vector<int> &, void(*func)(int));
-void print(int i);
+void forEach(std::vector<int>& arr, void (*callback)(int&));
+void print(int& i);
+void increment(int& i);
 
 int main() {
     std::vector<int> arr = {1, 2, 3, 4, 5};
+    forEach(arr, increment);
     forEach(arr, print);
     return 0;
 }
 
-void forEach(const std::vector<int> & arr, void(*callback)(int)) {
-    for (int i : arr)
-        callback(i);
+void forEach(std::vector<int>& arr, void (*callback)(int&)) {
+    for (int i = 0; i < arr.size(); i++)
+        callback(arr[i]);
 }
 
-void print(int i) {
+void increment(int& i) {
+    i++;
+}
+
+void print(int& i) {
     std::cout << i << " ";
 }
