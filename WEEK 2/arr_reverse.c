e1d81e11b876b7408b846d98315f0e11459fc7f1
EJBEAN2op <danonymousarmy@gmail.com>
2021-11-17 22:45:35 +0530

    arr reverse swagball

---
+++ WEEK 2/arr_reverse.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*)malloc(num * sizeof(int));
+    for (i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+    int mid = num / 2;
+    for (i = 0; i < mid; i++) {
+        int temp = arr[i];
+        arr[i] = arr[num - i - 1];
+        arr[num - i - 1] = temp;
+    }
+
+    for (i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
